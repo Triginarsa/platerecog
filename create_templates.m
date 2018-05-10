cfb7408475619926b0f60c9bf3ff3bb577af9ddb
A <mankadi123@gmail.com>
2018-05-10 10:03:05 +0800

    new dataset

--- create_templates.m
+++ create_templates.m
@@ -1,24 +1,24 @@
 %CREATE TEMPLATES
 %Letter
-A=imread('letters_numbers\A.bmp');B=imread('letters_numbers\B.bmp');
-C=imread('letters_numbers\C.bmp');D=imread('letters_numbers\D.bmp');
-E=imread('letters_numbers\E.bmp');F=imread('letters_numbers\F.bmp');
-G=imread('letters_numbers\G.bmp');H=imread('letters_numbers\H.bmp');
-I=imread('letters_numbers\I.bmp');J=imread('letters_numbers\J.bmp');
-K=imread('letters_numbers\K.bmp');L=imread('letters_numbers\L.bmp');
-M=imread('letters_numbers\M.bmp');N=imread('letters_numbers\N.bmp');
-O=imread('letters_numbers\O.bmp');P=imread('letters_numbers\P.bmp');
-Q=imread('letters_numbers\Q.bmp');R=imread('letters_numbers\R.bmp');
-S=imread('letters_numbers\S.bmp');T=imread('letters_numbers\T.bmp');
-U=imread('letters_numbers\U.bmp');V=imread('letters_numbers\V.bmp');
-W=imread('letters_numbers\W.bmp');X=imread('letters_numbers\X.bmp');
-Y=imread('letters_numbers\Y.bmp');Z=imread('letters_numbers\Z.bmp');
+A=imread('datasets\A.bmp');B=imread('datasets\B.bmp');
+C=imread('datasets\C.bmp');D=imread('datasets\D.bmp');
+E=imread('datasets\E.bmp');F=imread('datasets\F.bmp');
+G=imread('datasets\G.bmp');H=imread('datasets\H.bmp');
+I=imread('datasets\I.bmp');J=imread('datasets\J.bmp');
+K=imread('datasets\K.bmp');L=imread('datasets\L.bmp');
+M=imread('datasets\M.bmp');N=imread('datasets\N.bmp');
+O=imread('datasets\O.bmp');P=imread('datasets\P.bmp');
+Q=imread('datasets\Q.bmp');R=imread('datasets\R.bmp');
+S=imread('datasets\S.bmp');T=imread('datasets\T.bmp');
+U=imread('datasets\U.bmp');V=imread('datasets\V.bmp');
+W=imread('datasets\W.bmp');X=imread('datasets\X.bmp');
+Y=imread('datasets\Y.bmp');Z=imread('datasets\Z.bmp');
 %Number
-one=imread('letters_numbers\1.bmp');  two=imread('letters_numbers\2.bmp');
-three=imread('letters_numbers\3.bmp');four=imread('letters_numbers\4.bmp');
-five=imread('letters_numbers\5.bmp'); six=imread('letters_numbers\6.bmp');
-seven=imread('letters_numbers\7.bmp');eight=imread('letters_numbers\8.bmp');
-nine=imread('letters_numbers\9.bmp'); zero=imread('letters_numbers\0.bmp');
+one=imread('datasets\1.bmp');  two=imread('datasets\2.bmp');
+three=imread('datasets\3.bmp');four=imread('datasets\4.bmp');
+five=imread('datasets\5.bmp'); six=imread('datasets\6.bmp');
+seven=imread('datasets\7.bmp');eight=imread('datasets\8.bmp');
+nine=imread('datasets\9.bmp'); zero=imread('datasets\0.bmp');
 %*-*-*-*-*-*-*-*-*-*-*-
 letter=[A B C D E F G H I J K L M...
     N O P Q R S T U V W X Y Z];
